35b297e82fb23f60974db7899c9729b8a4c76383
ElenaLj <elenaljubijankic@gmail.com>
2021-12-01 12:34:48 +0100

    insert messaggi dinamici (da sistemare)

--- js/script.js
+++ js/script.js
@@ -6,7 +6,8 @@ const app = new Vue({
                 name: 'Michele',
                 avatar: '_1',
                 visible: true,
-                messages: [{
+                messages: [
+                {
                     date: '10/01/2020 15:30:55',
                     message: 'Hai portato a spasso il cane?',
                     status: 'sent'
@@ -89,6 +90,11 @@ const app = new Vue({
         setActiveContact: function(index) {
             //console.log("selected index is ", index);
             this.currentIndex = index;
+        },
+        addActiveClass: function(index) {
+            if(index == this.currentIndex) {
+                return 'active';
+            } 
         }
     }
 });
